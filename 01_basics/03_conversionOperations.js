3defe102afaf4ebc784614d1520fcec199a5ecdb
adarshtiwari786 <145568857+adarshtiwari786@users.noreply.github.com>
2025-01-24 15:51:48 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+/*let score = '33'
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc","Name" => NaN
+// "true" => 1; "false" =>0
+// undefined => NaN
+// null=> 0
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1=>true; 0=> false
+// "" =>false
+// "adarsh" =>true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
