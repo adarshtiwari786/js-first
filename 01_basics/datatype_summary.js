aeb16370b4f8a116307577f2041c9d6e9d837f42
adarshtiwari786 <145568857+adarshtiwari786@users.noreply.github.com>
2025-01-24 16:56:51 +0000

    datatype_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String , Number , Boolean , null , undefined
+// Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 36985522369853636985n
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman"+"naagraj"+"doga"]
+let myObj = {
+    name: "Adarsh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
